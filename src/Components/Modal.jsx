9063dcc7997f25725a1f7ba648cea9dd9cfca08c
Ali Aalam <aliaalam.me@gmail.com>
2024-03-13 23:52:40 +0500

    a

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -688,7 +688,7 @@ const StepThree = ({ nextStep }) => {
 
       <div className='grid grid-cols-12 items-center mx-auto w-full px-2'>
 
-        <div className='col-span-12 md:col-span-6 w-full relative px-2 mt-5 border rounded-xl flex justify-center py-4'>
+        <div className='col-span-12 md:col-span-6 w-full relative px-2 mt-5 border rounded-xl md:flex md:justify-center py-4'>
 
 
           <div>
@@ -702,7 +702,7 @@ const StepThree = ({ nextStep }) => {
 
 
             <div className='w-full flex  justify-center'>
-              <div className="relative w-[350px]    backdrop-blur-lg bg-[#000] bg-opacity-10 rounded-xl border border-white border-opacity-20">
+              <div className="relative w-full md:w-[350px]    backdrop-blur-lg bg-[#000] bg-opacity-10 rounded-xl border border-white border-opacity-20">
                 <div className="absolute top-0 right-2 flex items-center space-x-2 mt-1 cursor-pointer ">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
                     <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
@@ -739,7 +739,7 @@ const StepThree = ({ nextStep }) => {
 
 
             <div className='w-full flex  justify-center mt-4'>
-              <div className="relative w-[350px]    backdrop-blur-lg bg-[#000] bg-opacity-10 rounded-xl border border-white border-opacity-20">
+              <div className="relative w-full md:w-[350px]    backdrop-blur-lg bg-[#000] bg-opacity-10 rounded-xl border border-white border-opacity-20">
                 <div className="absolute top-0 right-2 flex items-center space-x-2 mt-1 cursor-pointer ">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
                     <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
